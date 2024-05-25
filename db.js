8a01e6f58397e98a9191e01283420a28a88f37b4
Aparna <singh123aparna.gmail.com>
2024-05-25 21:35:40 +0530

    Changes mongodb connection

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose=require("mongoose");
+require('dotenv').config();
 // define the mongodb connection url
-const mongoURL = "mongodb://localhost:27017/hotels";
-//const mongoURL="mongodb+srv://singh123aparnasa:<QWERTY12345>@cluster0.4epnihq.mongodb.net/"
+//const mongoURL = process.env.MONGODB_URL_LOCAL
+const mongoURL=process.env.MONGODB_URL;
+
 mongoose.connect(mongoURL,
     {
         useNewUrlParser: true
